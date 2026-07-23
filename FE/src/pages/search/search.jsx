af4856cf6f3566bdd68fb47bbf5f437c898b1560
chaewoni925 <popuri1216@naver.com>
2026-07-23 17:19:57 +0900

    feat:음악으로 여행지 찾기 기능 제거

--- FE/src/pages/search/search.jsx
+++ FE/src/pages/search/search.jsx
@@ -16,35 +16,24 @@ const SearchPage = () => {
 
 
                 {/* 상단 글 */}
-                <section className="flex flex-col items-center mt-[40px]">
+                <section className="flex flex-col items-center mt-[80px]">
                     <h1 className="text-[23px]  font-bold ">오늘은 어떤 여행을 하고 싶나요?</h1>
                 </section>
-        <section className="flex flex-col items-center mt-[20px]">
+        <section className="flex flex-col items-center mt-[100px]">
                     {/* 박스 1: 장소로 음악 찾기 */}
-                    <div className="cursor-pointer bg-[#D9D9D9] p-4 rounded-lg h-50 w-[350px] mt-8 overflow-hidden relative"
+                    <div className="cursor-pointer bg-[#D9D9D9] p-4 rounded-lg h-[300px] w-[350px] mt-8 overflow-hidden relative"
                     onClick={() => navigate('/searchPlaceToMusic')} >
                         <div
                             className="absolute inset-0 bg-cover bg-center rounded-lg"
                             style={{ backgroundImage: `url(${street})` }}
                         ></div>
                         <div className="relative z-10 p-2">
-                            <h1 className="text-[18px] font-bold text-white mb-2 mt-20">장소로 음악 찾기</h1>
+                            <h1 className="text-[18px] font-bold text-white mb-2 mt-[180px]">장소로 음악 찾기</h1>
                             <p className="text-[11px] text-white">도시/ 지역 분위기를 기반으로 플레이리스트를 생성합니다</p>
                         </div>
                     </div>
 
-                    {/* 박스 2: 음악으로 여행지 찾기  */}
-                    <div className="cursor-pointer bg-[#D9D9D9] p-4 rounded-lg h-50 w-[350px] mt-8 overflow-hidden relative"
-                    onClick={() => navigate('/searchMusicToPlace')} >
-                        <div
-                            className="absolute inset-0 bg-cover bg-center rounded-lg"
-                            style={{ backgroundImage: `url(${wave})` }}
-                        ></div>
-                        <div className="relative z-10 p-2">
-                            <h1 className="text-[18px] font-bold text-white mb-2 mt-20">음악으로 여행지 찾기</h1>
-                            <p className="text-[11px] text-white">음악 분위기를 기반으로 여행지를 추천 합니다</p>
-                        </div>
-                    </div>
+            
                 </section>
 
 
